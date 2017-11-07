fb6d5670b0c2b0afb1c2ac5ee1a79c7e579f1ba4
duonglq <duonglaiquang@gmail.com>
2017-11-07 22:48:51 +0700

    bai 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params) # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -18,11 +19,10 @@ class UsersController < ApplicationController
     end
   end
 
-
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :gender, :dob, :password,
+    params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
   end
 end
